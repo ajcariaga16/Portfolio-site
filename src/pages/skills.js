6c9d2f377ec79f3b304beef8b6a790165b07c8a6
AJ Cariaga <ajcariaga16@gmail.com>
2019-10-02 20:36:28 -0400

    init commit

---
+++ src/pages/skills.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const Skills = () => {
+  return(
+    <div>
+      <h3>Web Development</h3>
+      <p>React, JavaScript, HTML, SCSS</p>
+      <h3>General</h3>
+      <p>Java, C, C++</p>
+    </div>
+  )
+}
